9f48b5f224911d0d1a646d7d6b8d7d254c02c29d
dev-nasreen <nasreen.akter34@gmail.com>
2022-06-28 12:41:43 +0600

    subcategory route model done

---
+++ controllers/sub.js
@@ -0,0 +1,41 @@
+const Sub = require('../models/sub');
+const slugify = require('slugify')
+
+exports.create = async (req, res) =>{
+    try{
+        const {name, parent} = req.body;
+        const sub = await new Sub ({name, parent, slug:slugify(name)}).save();
+        res.json(sub)
+    }catch (error){
+        console.log(error);
+        res.status(400).send("create Sub failed" )
+    }
+}
+exports.list = async (req, res) => res.json( await  Sub.find({}).sort({createdAt: -1}).exec());
+
+exports.read = async (req, res) =>{
+    let Sub = await Sub.findOne({slug: req.params.slug}).exec();
+    res.json(Sub)
+}
+exports.update = async (req, res) =>{
+  const {name} = req.body;
+  try{
+    const updated = await Sub.findOneAndUpdate(
+        {slug:req.params.slug}, 
+        {name, slug: slugify(name)}, 
+        {new: true}
+        );
+       res.json(updated) 
+  }catch (error){
+    console.log(error);
+    res.status(400).send("Sub update failed")
+  }
+}
+exports.remove = async (req, res) =>{
+   try{
+const deleted = await Sub.findOneAndDelete({slug: req.params.slug});
+res.json(deleted);
+   }catch (error){
+    console.log(error);
+   }
+}
